c5fca2691c20bb37efe433e8f8a102d4087bcc63
Laura <laura.e.vollmer@gmail.com>
2024-09-03 10:50:02 -0500

    styling the button submit

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -2,14 +2,20 @@ import React from "react";
 import Button from "./Button";
 
 const Rating = ({ number }) => {
-  
   return (
-    <div className="flex flex-row justify-evenly items-center justify-center">
-      <Button number={1} />
-      <Button number={2} />
-      <Button number={3} />
-      <Button number={4} />
-      <Button number={5} />
+    <div>
+      <div className="flex flex-row justify-evenly items-center justify-center">
+        <Button number={1} />
+        <Button number={2} />
+        <Button number={3} />
+        <Button number={4} />
+        <Button number={5} />
+      </div>
+      <div className="flex flex-row items-center justify-center text-center ">
+        <button className="bg-white text-dark-blue text-xl font-overpass px-20 py-4 rounded-full">
+          <p className="font-bold">SUBMIT</p>
+        </button>
+      </div>
     </div>
   );
 };
