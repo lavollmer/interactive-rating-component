a045f7c3eae97df309bae017ac0b2b80d627659a
Laura <laura.e.vollmer@gmail.com>
2024-09-03 10:32:02 -0500

    updating the background and styling

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,14 @@ import IconStar from "../src/assets/icon-star.svg";
 function App() {
   return (
     <>
-      <div>
-        <div>
-          <img src={IconStar} alt="icon-star" />
-        </div>
-        <div>
-          <h1>How did we do?</h1>
+      <div className="font-overpass bg-very-dark-blue screen">
+        <div className="flex flex-col bg-dark-blue rounded-lg">
+          <div className="bg-dark-blue rounded-full h-8 w-8">
+            <img src={IconStar} alt="icon-star" className="h-4 w-4" />
+          </div>
+          <div>
+            <h1 className="text-white text-4xl">How did we do?</h1>
+          </div>
         </div>
       </div>
     </>
